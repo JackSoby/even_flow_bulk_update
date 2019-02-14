942e11124d85c445361d24542375900faeeda9e4
Jack Sobocinski <sobojack@bowst.com>
2019-02-14 13:55:41 -0500

    add open line and gold open line

--- lib/even_flow_bulk_update.ex
+++ lib/even_flow_bulk_update.ex
@@ -14,21 +14,21 @@ defmodule EvenFlowBulkUpdate do
   """
   require IEx
 
-  def evenflow_import do
+  def evenflow_google_import do
     body =
       File.stream!("./data/evenflow-google-data.csv")
       |> CSV.decode!(headers: true)
       |> Stream.chunk_every(10)
       |> Stream.map(fn attributes ->
-        encode_and_send(attributes)
+        encode_and_send(attributes, :google)
       end)
       |> Stream.run()
   end
 
-  def encode_and_send(attributes) do
+  def encode_and_send(attributes, :google) do
     body =
       attributes
-      |> create_encoded_body
+      |> create_encoded_body_google
 
     token = "api_url"
 
@@ -42,7 +42,7 @@ defmodule EvenFlowBulkUpdate do
       )
   end
 
-  def create_encoded_body(attributes) do
+  def create_encoded_body_google(attributes) do
     attributes
     |> Enum.map(fn attribute ->
       case attribute["brand"] do
@@ -64,5 +64,143 @@ defmodule EvenFlowBulkUpdate do
     end)
     |> Enum.filter(fn prod -> prod end)
     |> Poison.encode!()
+  end
+
+  def evenflow_open_line_usa_import do
+    body =
+      File.stream!("./data/2019-open-line-usa.csv")
+      |> CSV.decode!(headers: true)
+      |> Stream.chunk_every(10)
+      |> Stream.map(fn attributes ->
+        encode_and_send(
+          attributes,
+          :open_line
+        )
+      end)
+      |> Stream.run()
+  end
+
+  def create_encoded_body_open_line(attributes) do
+    attributes
+    |> Enum.map(fn attribute ->
+      case attribute[" Standard  "] do
+        "" ->
+          %{
+            ID: attribute["Model "],
+            SalePrice: price(attribute[" Sale Price "])
+          }
+
+        _ ->
+          %{
+            ID: attribute["Model "],
+            SalePrice: price(attribute[" Sale Price "])
+            # msrp: price(attribute[" Standard  "])
+          }
+      end
+    end)
+    |> Enum.filter(fn prod -> prod end)
+    |> Poison.encode!()
+  end
+
+  def price(price) do
+    String.replace(price, "$", "")
+    |> String.trim()
+    |> String.to_float()
+  end
+
+  def encode_and_send(attributes, :open_line) do
+    body =
+      attributes
+      |> create_encoded_body_open_line
+
+    token = "api_key"
+
+    headers = [Authorization: "Bearer #{token}", "Content-Type": "application/json"]
+
+    response =
+      HTTPoison.put!(
+        "https://app.salsify.com/api/v1/orgs/org_id/products",
+        body,
+        headers
+      )
+
+    IO.puts("---DONE UPDATING---")
+
+    # IO.inspect(response)
+  end
+
+  def evenflow_gold_open_line_import do
+    body =
+      File.stream!("./data/gold-open-even-flow.csv")
+      |> CSV.decode!(headers: true)
+      |> Stream.chunk_every(10)
+      |> Stream.map(fn attributes ->
+        encode_and_send(
+          attributes,
+          :gold_open
+        )
+      end)
+      |> Stream.run()
+  end
+
+  def create_encoded_body_gold_open_line(attributes) do
+    attributes
+    |> Enum.map(fn attribute ->
+      case attribute["Model "] do
+        "63012312" ->
+          nil
+
+        "63012311" ->
+          nil
+
+        _ ->
+          check_for_id(attribute)
+      end
+    end)
+    |> Enum.filter(fn prod -> prod end)
+    |> Poison.encode!()
+  end
+
+  def check_for_id(attribute) do
+    case String.length(attribute["Model "]) do
+      0 ->
+        nil
+
+      1 ->
+        nil
+
+      2 ->
+        nil
+
+      3 ->
+        nil
+
+      _ ->
+        %{
+          ID: attribute["Model "],
+          usd: price(attribute[" MSRP "])
+        }
+    end
+  end
+
+  def encode_and_send(attributes, :gold_open) do
+    body =
+      attributes
+      |> create_encoded_body_gold_open_line
+
+    token = "api_token"
+
+    headers = [Authorization: "Bearer #{token}", "Content-Type": "application/json"]
+
+    response =
+      HTTPoison.put!(
+        "https://app.salsify.com/api/v1/orgs/org_id/products",
+        body,
+        headers
+      )
+
+    IO.puts("---DONE UPDATING---")
+
+    IO.inspect(response)
   end
 end
